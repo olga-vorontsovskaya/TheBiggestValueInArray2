9892d18ac7bd14b7612683a2b416e5aed8949658
olga-vorontsovskaya <o.vorontsovskaya@gmail.com>
2024-04-29 16:45:39 +0300

    Add project files.

---
+++ TheBiggestValueInArray2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheBiggestValueInArray2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 905, 26, -2147483648, 90, 14, 10000010, 0 };
+
+            Console.WriteLine("Minimum number is " + arr.Min());
+            Console.WriteLine("Maximum number is " + arr.Max());
+        }
+    }
+}
